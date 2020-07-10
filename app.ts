6a1f9e2affd6ae79860ff98ab593f93235ee29ec
amir1004 <amir1004@gmail.com>
2020-07-10 16:46:33 +0300

    123

--- app.ts
+++ app.ts
@@ -5,19 +5,20 @@ var cors = require('cors')
 const compression = require('compression')
 const morgan = require('morgan')
 const path = require('path')
-
+console.log("am i here?")
 const normalizePort: any = (port: any) => parseInt(port, 10)
 const PORT = normalizePort(process.env.port)
 const app: Application = express();
 const dev = app.get('env') !== 'production'
 
 if (!dev) {
+    console.log("am i here?")
     app.disable('x-powered-by')
     app.use(compression())
     app.use(morgan('common'))
 
     app.use(express.static(path.resolve(__dirname, 'build')))
-
+    console.log("am i here?")
     app.get("/", (req: Request, res: Response, next: NextFunction) => {
 
         res.sendfile(path.resolve(__dirname, 'build', 'index.html'))
@@ -152,78 +153,22 @@ app.delete("/:id/delete", (req: Request, res: Response, next: NextFunction) => {
 app.get("/:index", (req: Request, res: Response, next: NextFunction) => {
 
 
-
+    console.log("am i here?")
     var sql = "select * from task where id = ?"
     var params = [req.params.index]
-
+    console.log("am i here?")
     db.all(sql, params, (err: any, row: any) => {
         if (err) {
             res.status(400).json({ "error": err.message });
             return;
         }
-
+        console.log("am i here?")
         res.send(row)
     });
 });
 
 app.listen(PORT || 5000, () => console.log('ServerRunning'))
 
-
-
-var vip_arr: Array<Object> = [
-    {
-        "name": "דוגמא",
-        "worth": "דוגמא",
-        "date": "דוגמא",
-        "source": "דוגמא",
-        "country": "דוגמא",
-        "tel": "דוגמא",
-        "email": "דוגמא"
-    }, {
-        "name": "Bill Gates",
-        "worth": "$90B",
-        "date": "1.1.1955",
-        "source": "microsoft",
-        "country": "USA",
-        "tel": "0555999230",
-        "email": "nycity@gkam.co.iol"
-    }, {
-        "name": "Warren Buffett",
-        "worth": "$84B",
-        "date": "12.2.1931",
-        "source": "Berkshire Hathaway",
-        "country": "USA",
-        "tel": "05465366239",
-        "email": "expressjs@exco.com"
-    },
-    {
-        "name": "Mark Zuckerberg",
-        "worth": "$71B",
-        "date": "3.4.1984",
-        "source": "Facebook",
-        "country": "USA",
-        "tel": "05465366233",
-        "email": "amir1001@gmail.com"
-    },
-    {
-        "name": "Larry Ellison",
-        "worth": "$57B",
-        "date": "5.7.1945",
-        "source": "Sun, Oracle",
-        "country": "USA",
-        "tel": "0552556443",
-        "email": "zjnas@fmail.com"
-    },
-    {
-        "name": "Michael Bloomberg",
-        "worth": "$50B",
-        "date": "23.4.1942",
-        "source": "Bloomberg",
-        "country": "USA",
-        "tel": "0552556666",
-        "email": "zabit@nurmagomerov.com"
-    }
-]
 
 app.get("/api/users", (req, res, next) => {
     var sql = "select * from user"
